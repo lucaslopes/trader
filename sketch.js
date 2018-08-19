4a6235d4214cf0acab107df8bbc96f36d46a69d0
Lucas Lopes <lucaslopesf2@gmail.com>
2018-08-18 21:45:20 -0300

    Call 'fakeAPI' function inside 'setup()' to use p5.js functions

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
-let prices = fakeAPI(numberOfCandles)
 const numCandles = 80
+let prices  = []
 
 function setup() {
   createCanvas(800, 300)
 
   let padding = (1 / (width / (numCandles + 1))) / numCandles * 1000
   let candleWidth = width / numCandles
+  prices  = fakeAPI(numCandles)
 }
 
 function draw() {
