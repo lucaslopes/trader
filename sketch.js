a769bcae4b9a0ff5fa18b901b1896461a145bbf2
Lucas Lopes <lucaslopesf2@gmail.com>
2018-08-18 21:43:42 -0300

    Let canvas wider and translate origin (0,0) to bottom left

--- sketch.js
+++ sketch.js
@@ -2,9 +2,13 @@ let prices = fakeAPI(numberOfCandles)
 const numCandles = 80
 
 function setup() {
+  createCanvas(800, 300)
 
 }
 
 function draw() {
+  background(51)
+  translate(0, height)
+  scale(1, -1)
 
 }
