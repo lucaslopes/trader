c320c738282e2e318eee4f55fbc35b6bf70778a8
Lucas Lopes <lucaslopesf2@gmail.com>
2018-08-23 06:14:18 -0300

    Change the Neural Network class to a Recurrente N.N.
    Creating a new input neuron, that receive the dot product and tanh
    activation between outputs and its new weights.

--- libraries/nn-copy.js
+++ libraries/nn-copy.js
@@ -1,3 +1,5 @@
+// Other techniques for learning
+
 class ActivationFunction {
   constructor(func, dfunc) {
     this.func = func;
@@ -15,48 +17,44 @@ let tanh = new ActivationFunction(
   y => 1 - (y * y)
 );
 
+
 class NeuralNetwork {
+  // TODO: document what a, b, c are
   constructor(a, b, c) {
     if (a instanceof NeuralNetwork) {
-      this.input_nodes  = a.input_nodes;
+      this.input_nodes = a.input_nodes;
       this.hidden_nodes = a.hidden_nodes;
       this.output_nodes = a.output_nodes;
-      this.memory_node  = a.memory_node;
 
       this.weights_ih = a.weights_ih.copy();
       this.weights_ho = a.weights_ho.copy();
-      this.weights_om = a.weights_om.copy();
 
       this.bias_h = a.bias_h.copy();
       this.bias_o = a.bias_o.copy();
-      this.bias_m = a.bias_m.copy();
     } else {
-      this.input_nodes  = a + 1;
+      this.input_nodes = a;
       this.hidden_nodes = b;
       this.output_nodes = c;
-      this.memory_node  = 0;
 
       this.weights_ih = new Matrix(this.hidden_nodes, this.input_nodes);
       this.weights_ho = new Matrix(this.output_nodes, this.hidden_nodes);
-      this.weights_om = new Matrix(                1, this.output_nodes);
       this.weights_ih.randomize();
       this.weights_ho.randomize();
-      this.weights_om.randomize();
 
       this.bias_h = new Matrix(this.hidden_nodes, 1);
       this.bias_o = new Matrix(this.output_nodes, 1);
-      this.bias_m = new Matrix(                1, 1);
       this.bias_h.randomize();
       this.bias_o.randomize();
-      this.bias_m.randomize();
     }
 
+    // TODO: copy these as well
     this.setLearningRate();
     this.setActivationFunction();
+
+
   }
 
   predict(input_array) {
-    input_array.push(this.memory_node)
 
     // Generating the Hidden Outputs
     let inputs = Matrix.fromArray(input_array);
@@ -70,11 +68,6 @@ class NeuralNetwork {
     output.add(this.bias_o);
     output.map(this.activation_function.func);
 
-    this.memory_node = Matrix.multiply(this.weights_om, output);
-    this.memory_node.add(this.bias_m);
-    this.memory_node.map(this.activation_function.func);
-    this.memory_node = this.memory_node.data[0][0]
-
     // Sending back to the caller!
     return output.toArray();
   }
@@ -83,7 +76,7 @@ class NeuralNetwork {
     this.learning_rate = learning_rate;
   }
 
-  setActivationFunction(func = tanh) {
+  setActivationFunction(func = sigmoid) {
     this.activation_function = func;
   }
 
@@ -112,6 +105,7 @@ class NeuralNetwork {
     let gradients = Matrix.map(outputs, this.activation_function.dfunc);
     gradients.multiply(output_errors);
     gradients.multiply(this.learning_rate);
+
 
     // Calculate deltas
     let hidden_T = Matrix.transpose(hidden);
@@ -144,37 +138,41 @@ class NeuralNetwork {
     // error.print();
   }
 
-  static deserialize(data) {
-    if (typeof data == 'string')
-      data = JSON.parse(data);
+  serialize() {
+    return JSON.stringify(this);
+  }
 
+  static deserialize(data) {
+    if (typeof data == 'string') {
+      data = JSON.parse(data);
+    }
     let nn = new NeuralNetwork(data.input_nodes, data.hidden_nodes, data.output_nodes);
-      nn.weights_ih = Matrix.deserialize(data.weights_ih);
-      nn.weights_ho = Matrix.deserialize(data.weights_ho);
-      nn.bias_h     = Matrix.deserialize(data.bias_h);
-      nn.bias_o     = Matrix.deserialize(data.bias_o);
-      nn.learning_rate = data.learning_rate;
+    nn.weights_ih = Matrix.deserialize(data.weights_ih);
+    nn.weights_ho = Matrix.deserialize(data.weights_ho);
+    nn.bias_h = Matrix.deserialize(data.bias_h);
+    nn.bias_o = Matrix.deserialize(data.bias_o);
+    nn.learning_rate = data.learning_rate;
     return nn;
   }
 
-  serialize() { return JSON.stringify(this); }
 
   // Adding function for neuro-evolution
-  copy() { return new NeuralNetwork(this); }
+  copy() {
+    return new NeuralNetwork(this);
+  }
 
   mutate(rate) {
     function mutate(val) {
       if (Math.random() < rate) {
         // return 2 * Math.random() - 1;
         return val + randomGaussian(0, 0.1);
-      } else return val;
+      } else {
+        return val;
+      }
     }
-
     this.weights_ih.map(mutate);
     this.weights_ho.map(mutate);
-    this.weights_om.map(mutate);
     this.bias_h.map(mutate);
     this.bias_o.map(mutate);
-    this.bias_m.map(mutate);
   }
 }
