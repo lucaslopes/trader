e9a0817ca533a9d08f1f8c0da2e0f7e9dbd96c6c
Lucas Lopes <lucaslopesf2@gmail.com>
2018-08-23 05:57:31 -0300

    Add Trader Class
    Is the agent that decide when, and how much, to make an trade operation.

---
+++ Trader.js
@@ -0,0 +1,99 @@
+class Trader {
+  constructor(i) {
+    this.brain = new NeuralNetwork(2, 3, 3)
+    this.confidence = random(0, .5)
+    this.risk  = random(.01, .1)
+    this.index = i
+    this.bets  = 0
+    this.funds = {
+      btc: 1,
+      usd: 1000
+    },
+  }
+  think(candle) {
+    let inputs = this.normalizeInputs(candle)
+    let output = this.brain.predict(inputs)
+
+    let profit = output[0]
+    let action = output[1]
+    let stop   = output[2]
+
+    profit = map(profit, 0, 1, 0, this.risk)
+    stop   = map(stop,   0, 1, 0, this.risk)
+
+    if (action > (1 - this.confidence) && this.funds.btc > 0)
+      return this.sell(candle.close, profit, stop)
+    else if (action < this.confidence && this.funds.usd > 0)
+      return this.buy(candle.close, profit, stop)
+    else return null
+  }
+
+  normalizeInputs(candle) {
+    let inputs = []
+
+    let o = candle.open
+    let c = candle.close
+    let dif = c - o
+    if (dif < 0) dif *= -1
+
+    let greater = c
+    if (o > greater) greater = o
+
+    inputs[0] = dif / greater
+
+    let extention = candle.hight - candle.low
+    let dif2 = extention - dif
+
+    inputs[1] = dif2 / extention
+
+    return inputs
+  }
+
+  sell(price, profit, stop) {
+    profit = price * (1 - stop)
+    stop   = price * (1 + stop)
+
+    let newOrder = {
+      owner: this.index,
+      type: 'buy',
+      stop: stop,
+      profit: profit,
+      value: this.funds.btc * price
+    }
+    this.apostas++
+    this.funds.btc = 0
+    return newOrder
+
+  }
+
+  buy(price, profit, stop) {
+    profit = price * (1 + stop)
+    stop   = price * (1 - stop)
+
+    let newOrder = {
+      owner: this.index,
+      type: 'sell',
+      stop: stop,
+      profit: profit,
+      value: this.funds.usd / price
+    }
+    this.apostas++
+    this.funds.usd = 0
+    return newOrder
+  }
+
+  deposit(coin, value) {
+    switch (coin) {
+      case 'btc':
+        this.funds.btc = value
+        break;
+
+      case 'usd':
+        this.funds.usd = value
+        break;
+
+      default:
+        console.log('Deposit error.')
+    }
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 const numCandles = 80
+const numTraders = 1000
 let prices  = []
 let candles = []
+let orders  = []
+let traders = []
 
 function setup() {
   createCanvas(800, 300)
@@ -10,6 +13,9 @@ function setup() {
 
   prices  = fakeAPI(numCandles)
   candles = createCandles(prices, candleWidth, padding)
+
+  for (var i = 0; i < numTraders; i++)
+    traders[i] = new Trader(i)
 }
 
 function draw() {
@@ -17,8 +23,20 @@ function draw() {
   translate(0, height)
   scale(1, -1)
 
-  for (var i = 0; i < candles.length; i++)
+  for (var i = 0; i < candles.length; i++) {
+    checkOrders(candles[i])
+
     candles[i].show(i)
+    if (i < numCandles * 2 / 3) {
+      for (trader of traders) {
+        let order = trader.think(candles[i])
+        if (order) orders.push(order)
+      }
+    }
+  }
+
+  showBest()
+  // noLoop()
 }
 
 function createCandles(prices, candleWidth, padding) {
@@ -26,8 +44,8 @@ function createCandles(prices, candleWidth, padding) {
   let higher = 0
 
   for (let i = 0; i < prices.length; i++) {
-    if (prices[i].hight > higher)
-      higher = prices[i].hight
+    if (prices[i].high > higher)
+      higher = prices[i].high
     if (prices[i].low < lower)
       lower = prices[i].low
   }
@@ -36,7 +54,7 @@ function createCandles(prices, candleWidth, padding) {
 
   for (let i = 0; i < prices.length; i++) {
     let o = map(prices[i].open,  lower, higher, 0, height)
-    let h = map(prices[i].hight, lower, higher, 0, height)
+    let h = map(prices[i].high,  lower, higher, 0, height)
     let l = map(prices[i].low,   lower, higher, 0, height)
     let c = map(prices[i].close, lower, higher, 0, height)
 
@@ -44,4 +62,52 @@ function createCandles(prices, candleWidth, padding) {
   }
 
   return candles
+}
+
+function checkOrders(candle) {
+  for (let i = orders.length - 1; i >= 0; i--) {
+    let o = orders[i]
+
+    if (o.stop > candle.low && o.stop < candle.hight) {
+      confirm(o, o.stop)
+      orders.splice(i, 1)
+    } else if (o.profit > candle.low && o.profit < candle.hight) {
+      confirm(o, o.profit)
+      orders.splice(i, 1)
+    }
+  }
+}
+
+function confirm(order, price) {
+  switch (order.type) {
+    case 'buy':
+      traders[order.owner].deposit('btc', order.value / price)
+      break;
+
+    case 'sell':
+      traders[order.owner].deposit('usd', order.value * price)
+      break;
+
+    default:
+      console.log('Not confirmed.')
+  }
+}
+
+function showBest() {
+  // console.log(orders);
+  // console.log(traders);
+
+  let maxBTC = 0
+  let maxUSD = 0
+  let maxAPO = 0
+
+  for(trader of traders) {
+    if (trader.funds.btc > maxBTC) maxBTC = trader.funds.btc
+    if (trader.funds.usd > maxUSD) maxUSD = trader.funds.usd
+    if (trader.apostas > maxAPO) maxAPO = trader.apostas
+  }
+
+  console.log('btc: ' + maxBTC, 'usd: ' + maxUSD);
+  // console.log(maxAPO);
+  // rect(width/2,0,20,height/10)
 }
